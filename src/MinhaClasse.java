448ae450deebee286223e05e32d58c32789e7f1e
Alexandre <xandreveras@gmail.com>
2023-10-04 12:55:10 -0300

    first commit

---
+++ src/MinhaClasse.java
@@ -0,0 +1,23 @@
+public class MinhaClasse {
+    
+    public static void main (String [] args){
+
+        System.out.print ("Hello World!!");//Exibir no terminal a mensagem
+
+
+        String primeiroNome = "Alexandre";
+        String ultimoNome = "Veras";
+        int minhaIdade = 26;
+        boolean verdadeira = true;
+        double minhaAltura = 1.91;
+
+        String nomecompleto = nomeCompleto(primeiroNome, ultimoNome);
+
+        System.out.print(nomecompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String ultimoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(ultimoNome); //concatenar nome completo
+    }
+}
