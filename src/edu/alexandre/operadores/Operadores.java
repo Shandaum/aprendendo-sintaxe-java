bb2df83cf43414e972f220493b8fe724ae40d292
Alexandre <xandreveras@gmail.com>
2023-10-04 18:08:41 -0300

    adição de novo pacote de operação

---
+++ src/edu/alexandre/operadores/Operadores.java
@@ -0,0 +1,23 @@
+package edu.alexandre.operadores;
+
+
+public class Operadores {
+    
+    public static void main(String[] args) throws Exception{
+    
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3; //resto
+        double resultado = (10 * 7) + (20/4); //operação igual na matemática básica
+
+        System.out.print(soma);
+        System.out.print(subtracao);
+        System.out.print(multiplicacao);
+        System.out.print(divisao);
+        System.out.print(modulo);
+        System.out.print(resultado);
+                
+    }
+}
--- src/edu/alexandre/tiposvariaveis/TipoVariaveis.java
+++ src/edu/alexandre/tiposvariaveis/TipoVariaveis.java
@@ -2,4 +2,22 @@ package edu.alexandre.tiposvariaveis;
 
 public class TipoVariaveis {
     
+    public static void main(String[] args) throws Exception{
+    
+    String nome = "Alexandre";
+    int idade = 26;
+    double altura = 1.91;
+    boolean verdadeiro = true;
+
+    
+    System.out.print(idade);
+    //variaveis podem mudar durante a execução
+    idade = 27;
+    System.out.print("Nova idade é " + idade);
+
+    //para a variavel ser reconhecida como constate a declaração é a seguinte
+
+        final int DATA_NASCIMENTO = 1997;
+
+    }
 }
